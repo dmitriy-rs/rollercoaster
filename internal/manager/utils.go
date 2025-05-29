4869886d3b65e296639d957b5ec5d16f7d8a51f6
Dmitriy Popov <popov.dmitriiy@gmail.com>
2025-05-29 17:59:11 +0200

    Added new github workflow and build modes

--- internal/manager/utils.go
+++ internal/manager/utils.go
@@ -4,16 +4,22 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/dmitriy-rs/rollercoaster/internal/logger"
 )
+
+var commandTextStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#6b9bd1")).
+	Bold(true)
 
 func CommandExecute(cmd *exec.Cmd, args ...string) {
 	if len(args) > 0 {
 		cmd.Args = append(cmd.Args, args...)
 	}
 
-	logger.Debug(fmt.Sprintf("Executing task: %s", cmd.Args))
+	logger.Info(fmt.Sprintf("Executing: %s", commandTextStyle.Render(strings.Join(cmd.Args, " "))))
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
